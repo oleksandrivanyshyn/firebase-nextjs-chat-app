81bc0e72ec5cdb29ca11b51df9e2cd3402daa3ea
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-11 22:11:10 +0300

    chore: update navigation imports, add theme attribute, and enhance register page layout

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { AvatarGenerator } from 'random-avatar-generator';
 
 const Page = () => {
@@ -24,7 +24,11 @@ const Page = () => {
   }, []);
   return (
     <div className="flex justify-center items-center h-screen p-10 m-2">
-      register
+      <form className="space-y-4 w-full max-w-2xl shadow-lg p-10">
+        <h1 className="text-xl text-center font-semibold text-[#0b3a65ff]">
+          Chat<span className="font-bold text-[#eeab63ff]">2</span>Chat
+        </h1>
+      </form>
     </div>
   );
 };
